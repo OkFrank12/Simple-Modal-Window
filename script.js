e769a5732e0e9fd4593d84b5556bbf6f3d6b95d3
Excel <excelwithfrank@gmail.com>
2024-11-25 13:38:30 +0100

    Updated the script file

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Manipulate the 3 buttons elements.
+// Manipulate Overlay and Popup.
+// Keypress Event using the addEventListener.
+
+// Select Elements.
+const btnsOpenModal = document.querySelectorAll(".btn");
+const btnCloseModal = document.querySelector(".close-btn");
+const overlayEl = document.querySelector(".overlay");
+const popupEl = document.querySelector(".popup");
+
+// btnsOpenModal.
